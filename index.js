f376e97580e9f915ec49b2adbdbff3778e80c2ac
Aman Prakash <amanprakash.lnct@gmail.com>
2022-10-12 23:04:19 +0530

    added styles

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
-for(var i=0; i<7; i++){
-document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    this.style.color="black";
-   console.log(this.innerHTML)
-   
-//    var audio= new Audio('sounds/crash.mp3');
-//     audio.play()
-//     document.querySelector(".q").classList.add("pressed")
-    //document.querySelector(".q").classList.remove("pressed")
-});
+for (var i = 0; i < 7; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    console.log(this.innerHTML);
+    makeSound(this.innerHTML);
+    animation(this.innerHTML);
+  });
 }
+
+document.addEventListener("keydown", function () {
+  console.log(event.key)
+  makeSound(event.key);
+  animation(event.key);
+});
+// document.addEventListener("keyup", function () {
+//   animationRemove(event.key);
+// });
+
+function makeSound(key) {
+  switch (key) {
+    case "q":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "e":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "r":
+      var bass = new Audio("sounds/kick-bass.mp3");
+      bass.play();
+      break;
+    case "t":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "y":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "u":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    default:
+      alert("Please Press Defined keys");
+      break;
+  }
+}
+function animation(curr_key){
+document.querySelector("."+curr_key).classList.add("pressed")
+
+setTimeout(function(){
+  document.querySelector("."+curr_key).classList.remove("pressed")
+},100);
+}
+// function animationRemove(curr_key){
+//   document.querySelector("."+curr_key).classList.remove("pressed")
+//   }
